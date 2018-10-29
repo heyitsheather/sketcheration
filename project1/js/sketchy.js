bd9479d45bc0c710864550b9d964c393c2bb7872
heyitsheather <33466700+heyitsheather@users.noreply.github.com>
2018-10-29 11:46:20 +0100

    Add files via upload

---
+++ project1/js/sketchy.js
@@ -0,0 +1,60 @@
+var cols = 50
+var rows = 50
+
+
+
+for (var i=0; i< cols; i++){
+    for (var j=0; j< rows; j++){
+        i*
+
+    }}
+
+
+
+for (var j = 0; j < 13; j++) {
+    for (var i = 0; i < 12; i++) {
+        image(getImage("cute/GemOrange"), i*36, j*30, 40, 60);
+    }
+}
+
+
+
+    
+
+
+
+
+var drawingDot = {
+ //starting point of dot   
+    x:0,
+    y:0,
+//how big is the dot
+    width: 3,
+    height: 3,
+
+    drawMe: function () {
+        ctx.drawImage(celineImg, this.x, this.y, this.width, this.height );
+
+    },
+};
+
+document.onkeydown = function (event) {
+    switch (event.keyCode) {
+        case 37: //left
+        dottie.x -= 10;
+        break;
+
+        case 38: //
+        dottie.
+        break;
+
+        case 39:
+        dottie. 
+        break;
+
+        case 40:
+        dottie. 
+    
+}
+
+if (grayArea
